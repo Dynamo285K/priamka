01e08d6b015b04fd0d2f15ac4b840828278e8779
Dynamo <tomasolbert0412@gmail.com>
2023-03-07 22:33:20 +0100

    hotovo

--- priamka.py
+++ priamka.py
@@ -1,5 +1,7 @@
 from PIL import Image
-pic = Image.new("RGB", (250,250), 'white')
+
+
+pic = Image.new("RGB", (300,250), 'white')
 pixels = pic.load()
 
 
@@ -14,12 +16,77 @@ Bx = int(B[0])
 By = int(B[1])
 
 
-if int(Bx-Ax)>int(By-Ay):
+
+
+# if int(Bx-Ax)>int(By-Ay):
+#     k = (By-Ay)/(Bx-Ax)
+#     q = (Ay-k)*Ax
+#     for x in range(Ax,Bx):
+#         y = int(k*x*q)
+#         pixels[x,y]=(0,0,0)
+# if int(Bx-Ax)<int(By-Ay):
+#     k = (By-Ay)/(Bx-Ax)
+#     q = (Ay-k)*Ax
+#     for x in range(Ax,Bx):
+#         y = int(k*x*q)
+#         pixels[x,y]=(0,0,0)
+
+# if Ax < Bx:
+#
+#     k = (By-Ay)/(Bx-Ax)
+#     q = Ay-k*Ax
+#     for x in range(Ax, Bx):
+#         y = int(k*x*q)
+#         pixels[x,y] = (0, 0, 0)
+#
+# elif Ax == Bx:
+#     if Ay < By:
+#         for y in range(Ay, By):
+#             x = Ax
+#             pixels[x,y] = (0, 0, 0)
+#     elif Ay == By:
+#         x = Ax
+#         y = Ay
+#         pixels[x,y] = (0, 0, 0)
+#     else:
+#         for y in range(By, Ay):
+#             x = Ax
+#             pixels[x,y] = (0, 0, 0)
+#
+# else:
+#     k = (Ay-By)/(Ax-Bx)
+#     q = By-k*Bx
+#     for x in range(Bx, Ax):
+#         y = int(k*x+q)
+#         pixels[x,y] = (0, 0, 0)
+if Ax < Bx:
+
     k = (By-Ay)/(Bx-Ax)
-    q = (Ay-k)*Ax
+    q = Ay-k*Ax
     for x in range(Ax,Bx):
-        y = int(k*x*q)
-        pixels[x,y]=(0,0,0)
+        y = int(k*x+q)
+        pixels[x,y] = (0,0,0)
+
+elif Ax == Bx:
+    if Ay < By:
+        for y in range(Ay,By):
+            x = Ax
+            pixels[x,y] = (0,0,0)
+    elif Ay == By:
+        x = Ax
+        y = Ay
+        pixels[x,y] = (0,0,0)
+    else:
+        for y in range(By,Ay):
+            x = Ax
+            pixels[x,y] = (0,0,0)
+
+else:
+    k = (Ay-Ay) / (Ax-Bx)
+    q = By-k*Bx
+    for x in range(Bx,Ax):
+        y = int(k*x+q)
+        pixels[x,y] = (0,0,0)
 
 
 pic.show()
